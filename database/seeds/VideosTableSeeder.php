cbbfa09585a3cac7340028b782ddd127b852a170
Vishnu Gp <vishnu.aassk@gmail.com>
2019-03-07 20:26:08 +0530

    Added default data seeders

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+Use App\Video;
 
 class VideosTableSeeder extends Seeder
 {
@@ -11,6 +12,40 @@ class VideosTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Let's truncate our existing records to start from scratch.
+        Video::truncate();
+
+        // Seed data
+        $seedDatas = array(
+            array(
+                'Video1', 120, 1100,
+            ),
+            array(
+                'Video2', 80, 2000,
+            ),
+            array(
+                'Video3', 250, 900,
+            ),
+            array(
+                'Video4', 90, 600,
+            ),
+            array(
+                'Video5', 75, 700,
+            ),
+            array(
+                'Video6', 300, 3000,
+            ),
+            array(
+                'Video7', 200, 2200,
+            ),
+        );
+        // And now, let's create a few videos in our database:
+        foreach ($seedDatas as $seedData ) {
+            Video::create([
+                'id' => $seedData[0],
+                'video_size' => $seedData[1],
+                'viewers_count' => $seedData[2],
+            ]);
+        }
     }
 }
